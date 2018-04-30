58242d053493ed776ad1e6afa9674a1d1f33e45e
Suchitra Naik <susanaik@gmail.com>
2018-04-30 20:27:36 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all.each{
+      puts "#{@name}"
+    }
+  end 
+  
+  def self.clear_all
+  
+end 
